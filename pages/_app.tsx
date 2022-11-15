5f201c2195f223f7c27b9bcb341bedb478f0c7c1
Dat <nguyendat1411@gmail.com>
2022-11-15 21:48:26 +0700

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
+const client = new ApolloClient({
+  uri: 'https://tutorial.saleor.cloud/graphql/',
+  cache: new InMemoryCache()
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider>
 }
